a5b7d71ddf9adc70d21b37f57e7db797fdb0ef2b
Brady <brady.withers@outlook.com>
2018-02-28 20:39:03 -0700

    form working

--- takthat/client/src/App.js
+++ takthat/client/src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
 class App extends Component {
+
+  state = {
+    fields: {}
+  };
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +28,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div className="App">
+        <Form onChange={fields => this.onChange(fields)} />
+        <p>
+          {JSON.stringify(this.state.fields, null, 2)}
+        </p>
+      </div>
+
+
       </div>
     );
   }
