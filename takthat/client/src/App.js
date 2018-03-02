cb1465e66f8185adf489335d76ab8e1fe2f52846
raphatapi <raphaeltapioca10@gmail.com>
2018-03-01 19:44:05 -0700

    pages

--- takthat/client/src/App.js
+++ takthat/client/src/App.js
@@ -1,45 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Form from './Form';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import CreateTak from "./pages/CreateTak";
+import MainBoard from "./pages/MainBoard";
+import NoMatch from "./pages/NoMatch";
+import Nav from './components/Nav';
+import CreateUser from "./pages/CreateUser";
 
-class App extends Component {
+const App = () =>
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/createUser" component={CreateUser} />
+        <Route exact path="/mainBoard" component={MainBoard} />
+        <Route exact path="/createTak" component={CreateTak} />
+        <Route component={NoMatch}/>
+      </Switch>
+    </div>
+  </Router>;
 
-  state = {
-    fields: {}
-  };
-
-  onChange = updatedValue => {
-    this.setState({
-      fields: {
-        ...this.state.fields,
-        ...updatedValue
-      }
-    });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <div className="App">
-        <Form onChange={fields => this.onChange(fields)} />
-        <p>
-          {JSON.stringify(this.state.fields, null, 2)}
-        </p>
-      </div>
-
-
-      </div>
-    );
-  }
-}
 
 export default App;
