8b394dfebe7eaef9b387c42c9ad5f82237c0f6a2
ljreader <lawrencereader@outlook.com>
2018-03-03 12:02:12 -0700

    individual keys added

---
+++ takthat/faceKeys.js
@@ -0,0 +1,8 @@
+console.log('Facebook Keys is loaded');
+
+var facebookKeys = {
+    facebook_app_id: '1770615536324095',
+    facebook_app_secret: '147ecc9b7a17dc17bed3cebf264f859e'
+};
+
+module.exports = facebookKeys;
---
+++ takthat/gooKeys.js
@@ -0,0 +1,8 @@
+console.log('Google Keys is loaded');
+
+var googleKeys = {
+	client_ID: '1040490853975-4gml8hbel7b8716vckrvslhsik9nv6ih.apps.googleusercontent.com',
+	client_secret: '2i518lnhqZhBIW7hWgKWAmRt'
+}
+
+module.exports = googleKeys;
