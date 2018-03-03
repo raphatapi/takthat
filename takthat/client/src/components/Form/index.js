ce18d0fd25bd1f286c3d54e14ed36d0af7d3adde
raphatapi <raphaeltapioca10@gmail.com>
2018-03-02 20:46:12 -0700

    test

--- takthat/client/src/components/Form/index.js
+++ takthat/client/src/components/Form/index.js
@@ -1,1 +1,6 @@
-export { default } from "./Form";
+export * from "./Input";
+export * from "./TextArea";
+export * from "./FormBtn";
+export * from "./Label";
+export * from "./FormGroup";
+export * from "./Form";
