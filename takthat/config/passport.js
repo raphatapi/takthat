0ba6d91a3d2d47971140aff6e28eb5019043f02c
airetriton <wobbe_espinoza@hotmail.com>
2018-03-04 21:51:27 -0700

    nicole14

--- takthat/config/passport.js
+++ takthat/config/passport.js
@@ -1,38 +1,40 @@
 // local authentication
 
-var LocalStrategy    = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 
 
 // ---------------- Facebook authentication -------------------//
-// function facebook() {
-//   var facebook = require('faceKeys.js');
-//   var keys = require("./faceKeys.js");
-//   var t = new twitter(keys);
-var FacebookStrategy = require('passport-facebook').Strategy;
-var FACEBOOK_APP_ID = "<Insert Your Key Here>"
-var FACEBOOK_APP_SECRET = "<Insert Your Secret Key Here>";
+function facebook() {
+  var FacebookStrategy = require('passport-facebook').Strategy;
+  var keys = require("../faceKeys.js");
+  var face = new FacebookStrategy(keys);
+
+// var FACEBOOK_APP_ID = "<Insert Your Key Here>"
+// var FACEBOOK_APP_SECRET = "<Insert Your Secret Key Here>";
 // ---------------- / Facebook authentication / -------------------//
 
 
 // --------------- Twitter authentication ----------------------//
-// function twitter() {
-//   var twitterKeys = require('twitKeys.js');
-//   var keys = require("./twitKeys.js");
-//   var t = new twitter(keys);
-var TwitterStrategy = require('passport-twitter').Strategy;
-var TWITTER_CONSUMER_KEY = "<Insert Your Key Here>";
-var TWITTER_CONSUMER_SECRET = "<Insert Your Secret Key Here>";
+function twitter() {
+  var TwitterStrategy = require('passport-twitter').Strategy;
+  var keys2 = require("../twitKeys.js");
+  var twit = new TwitterStrategy(keys2);
+
+// var TWITTER_CONSUMER_KEY = "<Insert Your Key Here>";
+// var TWITTER_CONSUMER_SECRET = "<Insert Your Secret Key Here>";
 // --------------- / Twitter authentication / ----------------------//
 
 
 // ----------------- Google authentication -----------------------//
-// 	function google() {
-//  var googleKeys = require('gooKeys.js');
-//  var keys = require("./gooKeys.js");
-//  var t = new twitter(keys);
-var GOOGLE_CONSUMER_KEY = "<Insert Your Key Here>";
-var GOOGLE_CONSUMER_SECRET = "<Insert Your Secret Key Here>";
-var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy
+function google() {
+    var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+    var keys3 = require("../gooKeys.js");
+    var goog = new GoogleStretegy(keys3);
+
+
+// var GOOGLE_CONSUMER_KEY = "<Insert Your Key Here>";
+// var GOOGLE_CONSUMER_SECRET = "<Insert Your Secret Key Here>";
+
 //------------------ / Google authentication / -----------------------//
 
 
@@ -53,10 +55,11 @@ module.exports = function(passport) {
         });
     });
 
-     passport.use('login', new LocalStrategy({
+    passport.use('login', new LocalStrategy({
         usernameField : 'email',
         passReqToCallback : true 
     },
+    
     function(req, email, password, done) {
        process.nextTick(function() {
             User.findOne({ 'user.email' :  email }, function(err, user) {
@@ -73,10 +76,11 @@ module.exports = function(passport) {
 
     }));
 
-     passport.use('signup', new LocalStrategy({
+    passport.use('signup', new LocalStrategy({
         usernameField : 'email',
         passReqToCallback : true 
     },
+    
     function(req, email, password, done) {
 
         process.nextTick(function() {
@@ -94,193 +98,192 @@ module.exports = function(passport) {
                         newUser.user.email = req.body.email;
                         newUser.user.phone = req.body.phone;
                         newUser.user.password = newUser.generateHash(password);
-			            newUser.user.name	= ''
-			            newUser.user.address	= ''
+			            // newUser.user.name	= ''
+			            // newUser.user.address	= ''
                         newUser.save(function(err) {
                             if (err)
                                 throw err;
                             return done(null, newUser);
                         });
-                    }
+                    };
 
                 });
             } else {
                 var user = req.user;
-		        user.user.username    = req.body.username;
-                user.user.email    = email;
+		        user.user.firstName = req.body.firstName;
+                user.user.email = email;
                 user.user.password = user.generateHash(password);
-			    user.user.name	= ''
-			    user.user.address	= ''
+			    // user.user.name	= ''
+			    // user.user.address	= ''
 
                 user.save(function(err) {
                     if (err)
                         throw err;
-            	} else {
-					var user = req.user;
-					user.user.firstName = req.body.username;
-					user.user.email    = email;
-					user.user.password = user.generateHash(password);
-					user.user.name	= ''
-					user.user.address	= ''
+                	} else {
+    					var user = req.user;
+    					user.user.firstName = req.body.firstName;
+    					user.user.email = email;
+    					user.user.password = user.generateHash(password);
+    					// user.user.name	= ''
+    					// user.user.address	= ''
 
-                	user.save(function(err) {
-                if (err)
-                    throw err;
-                    return done(null, user);
-                });
-
-            }
-
+                    	user.save(function(err) {
+                        if (err)
+                            throw err;
+                            return done(null, user);
+                        });
+                    };
+            };
         });
+    };
 
-
-    }));
 
 // Use the FacebookStrategy within Passport.
 // Strategies in Passport require a `verify` function, which accept
 // credentials (in this case, an accessToken, refreshToken, and Facebook
 // profile), and invoke a callback with a user object.
-	passport.use(new FacebookStrategy({
-    		clientID: FACEBOOK_APP_ID,
-    		clientSecret: FACEBOOK_APP_SECRET,
+	passport.use(new FacebookStrategy(keys){
+    		// clientID: FACEBOOK_APP_ID,
+    		// clientSecret: FACEBOOK_APP_SECRET,
     		callbackURL: "http://localhost:8080/auth/facebook/callback"
   		},
   		function(req, accessToken, refreshToken, profile, done) {
     		// asynchronous verification, for effect...
 
-    			process.nextTick(function () {
-            		if (!req.user) {
- 					  User.findOne({ 'user.email' :  profile.emails[0].value }, function(err, user) {
-            	       if (err){ return done(err);}
-                    		if (user) {
-                        	   return done(null, user);
-                    		} else {
-                        		var newUser = new User();
-							     newUser.user.username = profile.displayName;
-                        				newUser.user.email    = profile.emails[0].value;
-							newUser.user.name	= ''
-							newUser.user.address	= ''
+			process.nextTick(function () {
+        		if (!req.user) {
+				    User.findOne({ 'user.email' :  profile.emails[0].value }, function(err, user) {
+        	        if (err){ return done(err);}
+                		if (user) {
+                    	   return done(null, user);
+                		} else {
+                    		var newUser = new User();
+						    newUser.user.firstName = profile.displayName;
+                    		newUser.user.email = profile.emails[0].value;
+						    newUser.user.name = ''
+						    newUser.user.address = ''
 
-                        				newUser.save(function(err) {
-                            					if (err)
-                                					throw err;
-                            				return done(null, newUser);
-                        				});
-                    				}
+                    		newUser.save(function(err) {
+                        		if (err)
+                            	throw err;
+                        		return done(null, newUser);
+                    		});
+                		}
 
-                			});
-                         	} else {
-					var user            = req.user;
-					user.user.username    = profile.displayName;
-                			user.user.email    = profile.emails[0].value;
-					user.user.name	= ''
-					user.user.address	= ''
+            	   });
+                } else {
+				   var user = req.user;
+				   user.user.firstName = profile.displayName;
+            	   user.user.email    = profile.emails[0].value;
+				   user.user.name	= ''
+				   user.user.address	= ''
 
-                			user.save(function(err) {
-                    				if (err)
-                        				throw err;
-                    			return done(null, user);
-                			});
-            			}
-    			});
+            	   user.save(function(err) {
+                    if (err)
+                    throw err;
+                	return done(null, user);
+            		});
+        		}
+			});
   		}
-	));
+	);
 
 // Use the TwitterStrategy within Passport.
 // Strategies in passport require a `verify` function, which accept
 // credentials (in this case, a token, tokenSecret, and Twitter profile), and
 // invoke a callback with a user object.
-			passport.use(new TwitterStrategy({
-    			consumerKey: TWITTER_CONSUMER_KEY,
-    			consumerSecret: TWITTER_CONSUMER_SECRET,
-    			callbackURL: "http://192.168.1.101:8080/auth/twitter/callback"
-  		},
-  		function(req,token, tokenSecret, profile, done) {
+	passport.use(new TwitterStrategy(keys2){
+		// consumerKey: TWITTER_CONSUMER_KEY,
+		// consumerSecret: TWITTER_CONSUMER_SECRET,
+		callbackURL: "http://192.168.1.101:8080/auth/twitter/callback"
+  	},
+  	function(req,token, tokenSecret, profile, done) {
     // asynchronous verification, for effect...
-    			process.nextTick(function () {
+		process.nextTick(function () {
       
-     				 if (!req.user) {
- 					User.findOne({ 'user.username' :  profile.displayName }, function(err, user) {
-            	    				if (err){ return done(err);}
-                    				if (user) {
-                        				return done(null, user);
-                    				} else {
-                        				var newUser            = new User();
-							newUser.user.username    = profile.displayName;
-					newUser.user.name	= ''
-					newUser.user.address	= ''
+     		if (!req.user) {
+ 				User.findOne({ 'user.username' :  profile.displayName }, function(err, user) {
+    				if (err){ return done(err);}
+    				if (user) {
+        				return done(null, user);
+    				} else {
+                        var newUser = new User();
+						newUser.user.firstName = profile.displayName;
+					    newUser.user.name = ''
+					    newUser.user.address = ''
 
-                        				newUser.save(function(err) {
-                            					if (err)
-                                					throw err;
-                            				return done(null, newUser);
-                        				});
-                    				}
+        				newUser.save(function(err) {
+            					if (err)
+                					throw err;
+            				return done(null, newUser);
+        				});
+                    }
 
-                			});
-                         	} else {
-					var user            = req.user;
-					user.user.username    = profile.displayName;
-					user.user.name	= ''
-					user.user.address	= ''
+                });
+                } else {
+					var user = req.user;
+					user.user.firstName = profile.displayName;
+					user.user.name= ''
+					user.user.address = ''
 
-                			user.save(function(err) {
-                    				if (err)
-                        				throw err;
-                    			return done(null, user);
-                			});
-            			}
-    			});
-  		}		
-	));
+        			user.save(function(err) {
+            				if (err)
+                				throw err;
+            			return done(null, user);
+        			});
+            	}
+    	});
+  	}
+    );
 
 // Use the GoogleStrategy within Passport.
 // Strategies in Passport require a `verify` function, which accept
 // credentials (in this case, an accessToken, refreshToken, and Google
 // profile), and invoke a callback with a user object.
-		passport.use(new GoogleStrategy({
-    				clientID: GOOGLE_CONSUMER_KEY,
-    				clientSecret: GOOGLE_CONSUMER_SECRET,
-    				callbackURL: "http://localhost:8080/auth/google/callback"
-  				},
-  				function(req, accessToken, refreshToken, profile, done) {
+	passport.use(new GoogleStrategy(keys3){
+		// clientID: GOOGLE_CONSUMER_KEY,
+		// clientSecret: GOOGLE_CONSUMER_SECRET,
+		callbackURL: "http://localhost:8080/auth/google/callback"
+	},
+	function(req, accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
-    					process.nextTick(function () {
-      
-     						if (!req.user) {
- 							User.findOne({ 'user.email' :  profile.emails[0].value }, function(err, user) {
-            	    						if (err){ return done(err);}
-                    					if (user) {
-                        					return done(null, user);
-                    					} else {
-                        					var newUser            = new User();
-								newUser.user.username    = profile.displayName;
-								newUser.user.email    = profile.emails[0].value;
-								newUser.user.name	= ''
-								newUser.user.address	= ''
+		process.nextTick(function () {
 
-                        					newUser.save(function(err) {
-                            						if (err)
-                                						throw err;
-                            					return done(null, newUser);
-                        					});
-                    					}
+				if (!req.user) {
+				User.findOne({ 'user.email' :  profile.emails[0].value }, function(err, user) {
+    				if (err){ return done(err);}
+    				if (user) {
+        			return done(null, user);
+    				} else {
+        				var newUser = new User();
+				        newUser.user.firstName = profile.displayName;
+				        newUser.user.email = profile.emails[0].value;
+				        newUser.user.name = ''
+				        newUser.user.address = ''
 
-                					});
-                         			} else {
-							var user            = req.user;
-							user.user.username    = profile.displayName;
-							user.user.email    = profile.emails[0].value;
-                					user.save(function(err) {
-                    					if (err)
-                        					throw err;
-                    					return done(null, user);
-							});
-						}
-                			});
+        				newUser.save(function(err) {
+            				if (err)
+                			throw err;
+            				return done(null, newUser);
+        				});
+    				}
 
-    			}
+				});
+         		} else {
+                    var user = req.user;
+                    user.user.firstName = profile.displayName;
+                    user.user.email = profile.emails[0].value;
+		            user.save(function(err) {
+    					if (err)
+        					throw err;
+    					return done(null, user);
+			        });
+		        }
+		});
+
+    }
  
-));
+    );
 
 };
+
