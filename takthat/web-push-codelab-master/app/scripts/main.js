443c52f083497be8dba1b0300222dccf4a2a409a
kferny1 <kferny1@gmail.com>
2018-03-10 11:24:33 -0700

    push notif

--- takthat/web-push-codelab-master/app/scripts/main.js
+++ takthat/web-push-codelab-master/app/scripts/main.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = '<BBJj73_ICzhjyMVFjSfSNymr0wpjgiwRqklIXlSni_eNxLHrE3vvGPnbEXDqwL23lFJoxp4G1FNLGqkIL9--NlM>';
 
 const pushButton = document.querySelector('.js-push-btn');
 
@@ -41,4 +41,28 @@ function urlB64ToUint8Array(base64String) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
+}
+function initializeUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
 }
