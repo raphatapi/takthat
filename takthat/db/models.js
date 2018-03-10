aaa46336f2ecf53e49c670371743938142b9d893
raphatapi <raphaeltapioca10@gmail.com>
2018-03-10 10:44:40 -0700

    test

--- takthat/db/models.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-var notesSchema = new mongoose.Schema({
-  text: String,
-  created_at: {type: Date, default: Date.now}
-});
-
-mongoose.model('Notes', notesSchema);
