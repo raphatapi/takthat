20fe81984be3b3738f29d16186bfbbc4f04ed260
raphatapi <raphaeltapioca10@gmail.com>
2018-03-10 10:30:23 -0700

    problem

--- tictac3/working/passportstuff/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    User: require("./user"),
-    Tak: require("./taks")
-  };
--- tictac3/working/passportstuff/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const userRoutes = require("./users");
-const takRoutes = require("./taks");
-
-// Article routes
-router.use("/users", userRoutes);
-router.use("/taks", takeRoutes);
-
-module.exports = router;
